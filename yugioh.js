7d62d01f6a371905d16c90ed4a2261cbc639ab0d
justin-t-5 <justintamayd12@gmail.com>
2026-01-20 23:36:38 -0500

    Implemented Card Grid

--- yugioh.js
+++ yugioh.js
@@ -17,24 +17,48 @@ async function getCards() {
 }
 getCards();
 
-async function searchCard(name){
+// async function searchCard(name){
+//     const card = allCards.filter(card => card.name.toLowerCase().includes(name.toLowerCase()));
+
+//     let cardimg = document.getElementsByTagName('img')[0];
+//     let cardimg1 = document.getElementsByTagName('img')[1];
+//     let cardimg2 = document.getElementsByTagName('img')[2];
+//     let cardimg3 = document.getElementsByTagName('img')[3];
+//     let cardimg4 = document.getElementsByTagName('img')[4];
+
+
+//     let cardname = document.getElementsByTagName('h4')[0];
+//     // Set the image source to the card's image URL
+//     cardimg.src = card[0].card_images[0].image_url;
+//     cardimg1.src = card[1].card_images[0].image_url;
+//     cardimg2.src = card[2].card_images[0].image_url;
+//     cardimg3.src = card[3].card_images[0].image_url;
+//     cardimg4.src = card[4].card_images[0].image_url;
+
+//     cardname.textContent = card[0].name;
+// }
+
+async function matchCards(name){
+    const grid = document.getElementById('cardGrid');
+    grid.innerHTML= '';
     const card = allCards.filter(card => card.name.toLowerCase().includes(name.toLowerCase()));
 
-    let cardimg = document.getElementsByTagName('img')[0];
-    let cardname = document.getElementsByTagName('h4')[0];
-    // Set the image source to the card's image URL
-    cardimg.src = card[0].card_images[0].image_url;
-    cardname.textContent = card[0].name;
+    for(let i = 0; i<card.length; i++){
+       const imgElement = document.createElement('img');
+       imgElement.classList.add('cardGrid');
+       imgElement.src = card[i].card_images[0].image_url;
+       grid.appendChild(imgElement);
+    }
+    
 }
 
 const form = document.getElementById("cardForm");
 const input = document.getElementById("cardInput");
-// const form = document.getElementById("cardForm");
-// const input = document.getElementById("cardInput");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop page reload
 
     const name = input.value;
-    searchCard(name);
+    // searchCard(name);
+    matchCards(name);
 });
