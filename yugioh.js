0931c9d10c7320020dad4c1c8fd377b5d106c3a4
justin-t-5 <justintamayd12@gmail.com>
2026-01-21 23:32:03 -0500

    Initial Modal Setup

--- yugioh.js
+++ yugioh.js
@@ -4,39 +4,8 @@ async function getCards() {
     const res = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php");
     const data = await res.json();
 
-    allCards = data.data;
-    // Filter for Dark Magician
-    // const result = data.data.filter(card => card.name.includes("Dark Magician"));
-
-    // // Get the first <img> tag on the page
-    // let cardimg = document.getElementsByTagName('img')[0];
-    // let cardname = document.getElementsByTagName('h4')[0];
-    // // Set the image source to the card's image URL
-    // cardimg.src = result[0].card_images[0].image_url;
-    // cardname.textContent = result[0].name;
-}
+    allCards = data.data;}
 getCards();
-
-// async function searchCard(name){
-//     const card = allCards.filter(card => card.name.toLowerCase().includes(name.toLowerCase()));
-
-//     let cardimg = document.getElementsByTagName('img')[0];
-//     let cardimg1 = document.getElementsByTagName('img')[1];
-//     let cardimg2 = document.getElementsByTagName('img')[2];
-//     let cardimg3 = document.getElementsByTagName('img')[3];
-//     let cardimg4 = document.getElementsByTagName('img')[4];
-
-
-//     let cardname = document.getElementsByTagName('h4')[0];
-//     // Set the image source to the card's image URL
-//     cardimg.src = card[0].card_images[0].image_url;
-//     cardimg1.src = card[1].card_images[0].image_url;
-//     cardimg2.src = card[2].card_images[0].image_url;
-//     cardimg3.src = card[3].card_images[0].image_url;
-//     cardimg4.src = card[4].card_images[0].image_url;
-
-//     cardname.textContent = card[0].name;
-// }
 
 async function matchCards(name){
     const grid = document.getElementById('cardGrid');
@@ -44,14 +13,25 @@ async function matchCards(name){
     const card = allCards.filter(card => card.name.toLowerCase().includes(name.toLowerCase()));
 
     for(let i = 0; i<card.length; i++){
-       const imgElement = document.createElement('img');
+       const imgElement = document.createElement('a');
        imgElement.classList.add('cardGrid');
-       imgElement.src = card[i].card_images[0].image_url;
+       imgElement.addEventListener('click', () => openModal())
+       const img = document.createElement('img');
+       img.src = card[i].card_images[0].image_url;
+       imgElement.appendChild(img);
        grid.appendChild(imgElement);
-    }
-    
+    }  }
+
+const close = document.getElementById('close');
+const modal = document.getElementById('my-modal');
+
+function openModal(){
+    modal.showModal();
 }
 
+close.addEventListener('click', () => {
+    modal.close();
+})
 const form = document.getElementById("cardForm");
 const input = document.getElementById("cardInput");
 
